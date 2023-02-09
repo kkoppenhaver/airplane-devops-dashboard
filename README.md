460c3663dfaf024a7411da9abc41a973472ad8f2
Keanan Koppenhaver <keanan@alphaparticle.com>
2023-02-09 01:35:23 -0600

    First working version of view_servers task, calling AWS API

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # DevOps Dashboard - Built with Airplane
+
+
+## TO-DO
+===
+
+  - Create task for actually spinning up servers
+  	+ Make sure all Airplane-created servers are tagged
+  - Make sure view task only displays Airplane-tagged servers
+  - Create View that runs the view task and shows the table at all times
+  - Create button in View to destroy a server
+  - Create button in View to create a new server
+  - Take input for server name, etc in the View
--- view_aws_servers_airplane.py
+++ view_aws_servers_airplane.py
@@ -1,22 +1,30 @@
 import airplane
-
+import boto3
 
 @airplane.task(
     slug="view_aws_servers",
     name="View AWS Servers",
 )
 def view_aws_servers():
-    data = [
-        {"id": 1, "name": "Gabriel Davis", "role": "Dentist"},
-        {"id": 2, "name": "Carolyn Garcia", "role": "Sales"},
-        {"id": 3, "name": "Frances Hernandez", "role": "Astronaut"},
-        {"id": 4, "name": "Melissa Rodriguez", "role": "Engineer"},
-        {"id": 5, "name": "Jacob Hall", "role": "Engineer"},
-        {"id": 6, "name": "Andrea Lopez", "role": "Astronaut"},
-    ]
+    ec2 = boto3.resource('ec2')
 
-    # Sort the data in ascending order by name.
-    data = sorted(data, key=lambda u: u["name"])
+    data = []
+
+    for instance in ec2.instances.all():
+        print(instance)
+        data.append({
+            "ID": instance.id,
+            "Amazon Machine Image (AMI)": instance.image_id,
+            "Type": instance.instance_type,
+            "IP Address": instance.public_ip_address,
+            "State": instance.state['Name']
+        })
+
+    # Sort the data in ascending order by ID.
+    data = sorted(data, key=lambda u: u["ID"])
+
+    if not data:
+        return [{"Output": 'No servers found.'}]
 
     # You can return data to show output to users.
     # Output documentation: https://docs.airplane.dev/tasks/output
